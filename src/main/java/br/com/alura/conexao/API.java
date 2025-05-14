8db85a525400dfd23437d0de20f4c692ead605ff
Carlos <carloswelliton7@gmail.com>
2025-05-13 21:44:42 -0400

    add .env file

--- src/main/java/br/com/alura/conexao/API.java
+++ src/main/java/br/com/alura/conexao/API.java
@@ -1,5 +1,26 @@
 package br.com.alura.conexao;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class API {
+  HttpClient client = HttpClient.newHttpClient();
+
+  HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create("https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT"))
+      .build();
+  {
+    try {
+      HttpResponse<String> response = client
+          .send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
 }
